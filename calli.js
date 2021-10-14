aa058e589f5a3a12dbc3703bf208231e2b9da4b2
brwa42 <79860367+brwa42@users.noreply.github.com>
2021-10-14 17:20:16 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -37,7 +37,7 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "s!";
+const prefix = "$";
 const cooldown = new Set();
 const cdtime = 5;
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,7 +66,7 @@ calli.on("message", message => {
   }
 });
 ///////////////////////////////////////////////////////////////////////////////
-calli.login("")
+calli.login("ODk4MjAyOTI2ODYwODc3ODI0.YWgyqQ.6G3N7JCdX2h0MMG2t56BpGwQpz0")
 ///////////////////////////////////////////////////////////////////////////////
 const callienabled  = "";
 const callidisabled = "";
